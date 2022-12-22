1e935f8cfd2b0ed2e47e6e9bdab65a7166cb2ab5
prottayRoyarnob <aarnob2020@gmail.com>
2022-12-22 20:59:23 +0600

    navbar

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import Navbar from './Pages/Shared/Navbar';
 import { Routes, Route, Outlet, Link } from "react-router-dom";
 import Home from './Pages/Home/Home';
 import About from './Pages/About/About';
+import Login from './Pages/Login/Login';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Navbar />
       <Routes>
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
+          <Route path="login" element={<Login />} />
       </Routes>
     </div>
   );
